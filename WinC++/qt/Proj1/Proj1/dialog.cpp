2d3f3445c3654f7a693b5678fc31907da390bc93
Bat <bat@man.pl>
2024-06-18 23:12:36 +0200

    QT wrrr!

--- WinC++/qt/Proj1/Proj1/dialog.cpp
+++ WinC++/qt/Proj1/Proj1/dialog.cpp
@@ -6,9 +6,33 @@ Dialog::Dialog(QWidget *parent)
     , ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+    this->pushButton = ui->pushButton;
+    this->textline1 = ui->lineEdit;
+    this->textline2 = ui->lineEdit_2;
+
+    this->textCRC = ui->label_3;
+    this->textCycle = ui->label_6;
+    this->textTime = ui->label_8;
+
+
+
 }
 
 Dialog::~Dialog()
 {
     delete ui;
 }
+
+
+
+void Dialog::on_pushButton_released()
+{
+    QString data = textline1->text();
+    QString cycleNumString = textline2->text();
+    long cycleNum = (long) cycleNumString;
+    this->textCRC->setText( data );
+    this->textCycle->setText( cycleNum );
+
+
+}
+
--- WinC++/qt/Proj1/Proj1/dialog.h
+++ WinC++/qt/Proj1/Proj1/dialog.h
@@ -2,6 +2,9 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +20,16 @@ public:
     Dialog(QWidget *parent = nullptr);
     ~Dialog();
 
+private slots:
+    void on_pushButton_released();
+
 private:
     Ui::Dialog *ui;
+    QPushButton *pushButton ;
+    QLabel      *textCRC;
+    QLabel      *textCycle;
+    QLabel      *textTime;
+    QLineEdit   *textline1;
+    QLineEdit   *textline2;
 };
 #endif // DIALOG_H
