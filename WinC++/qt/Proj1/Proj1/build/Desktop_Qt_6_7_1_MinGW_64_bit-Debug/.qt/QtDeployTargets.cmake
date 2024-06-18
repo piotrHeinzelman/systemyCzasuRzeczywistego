98f22a706dc60f73d69457f437a6a2fc92481c4a
Bat <bat@man.pl>
2024-06-18 22:29:41 +0200

    ?

---
+++ WinC++/qt/Proj1/Proj1/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Proj1_FILE C:/Users/Bill/Desktop/SystemyCzasuRzeczywistego/WinC++/qt/Proj1/Proj1/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/Proj1.exe)
+set(__QT_DEPLOY_TARGET_Proj1_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
--- WinC++/qt/Proj1/p1/build/Desktop_Qt_6_7_1_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_p1_FILE C:/Users/Bill/Desktop/SystemyCzasuRzeczywistego/WinC++/qt/Proj1/p1/build/Desktop_Qt_6_7_1_MinGW_64_bit-Release/p1.exe)
-set(__QT_DEPLOY_TARGET_p1_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
